4cd0f6b340fd97952a25743c6dbf4459c3c4d5bc
Andrew Alyamovsky <ddlzz@yandex.ru>
2017-11-24 17:56:51 +0300

    added fields for contact and company entities

--- src/Entities/Amo/Company.php
+++ src/Entities/Amo/Company.php
@@ -16,4 +16,54 @@ class Company extends BaseEntity implements EntityInterface
 {
     /** @var string */
     protected $requestName = 'companies';
+
+
+    /** @var array */
+    protected $fieldsParamsAppend = [
+        'name' => [
+            'type' => 'string',
+            'required_add' => true,
+            'required_update' => false,
+        ],
+        'updated_by' => [
+            'type' => 'int',
+            'required_add' => false,
+            'required_update' => false,
+        ],
+        'group_id' => [
+            'type' => 'int',
+            'required_add' => false,
+            'required_update' => false,
+        ],
+        'leads' => [
+            'type' => 'array',
+            'required_add' => false,
+            'required_update' => false,
+        ],
+        'closest_task_at' => [
+            'type' => 'int',
+            'required_add' => false,
+            'required_update' => false,
+        ],
+        'tags' => [
+            'type' => 'array|string', // [[id => int, name => string], [id => int, name => string]]
+            'required_add' => false,
+            'required_update' => false,
+        ],
+        'custom_fields' => [
+            'type' => 'array', // [[id => int, name => string, values => [value => string?, enum => string?]], ...]
+            'required_add' => false,
+            'required_update' => false,
+        ],
+        'contacts' => [
+            'type' => 'array',
+            'required_add' => false,
+            'required_update' => false,
+        ],
+        'customers' => [
+            'type' => 'array',
+            'required_add' => false,
+            'required_update' => false,
+        ],
+    ];
 }
